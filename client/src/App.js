7bc4a2f15cbdc7a6d3beddae3d19084d9048c53c
shivamgupta1319 <sg247938@gmail.com>
2022-06-27 18:07:39 +0530

    up

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useEffect, useState} from 'react'
+const req = require('axios');
 function App() {
+  useEffect(()=>{
+    req.get('http://localhost:8080/data/'+cityName).then((response)=>{
+      console.log(response.data);
+  })})
+  const [cityName,setCityname] = useState('')
+  const fun = (event) => {
+      setCityname(event.target.value);
+      console.log(event.target.value);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather Report</h1>
+        <input type="text" onChange={fun} value={cityName} name='city_name' placeholder='Enter city name'/><br></br><br></br><br></br>
+        <button  class="button-29" role="button" type='submit' value={"submit"} >submit</button>
+        
       </header>
     </div>
   );
