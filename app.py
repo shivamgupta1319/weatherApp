7974da3410b2fc98a8cdbc508393d6b89596ce36
shivamgupta1319 <sg247938@gmail.com>
2022-06-28 11:49:49 +0530

    up

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask_restful import Api
-from .database import config
-from flask import Flask, jsonify, render_template, request, send_from_directory
+from flask import Flask
 from .modules import weather_data,models
 from flask_cors import CORS
 
@@ -8,12 +7,6 @@ app = Flask(__name__, static_url_path='', static_folder='')
 api = Api(app)
 CORS(app, resources={r"*": {"origins": "*"}})
 models.db()
-
-
-
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'')
 
 
 api.add_resource(weather_data.weatherdata,'/data/<city_name>')
