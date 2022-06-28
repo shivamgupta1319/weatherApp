4c4750c69ef4dfe7b66a4e0958507131b36fc276
shivamgupta1319 <sg247938@gmail.com>
2022-06-28 11:52:12 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import BasicCard from './components/card';
 const req = require('axios');
 function App() {
